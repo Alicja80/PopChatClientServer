cd56199ec9b14cbd080e2de27db6e92f7a5f9008
Alicja80 <alicjalucas@gmail.com>
2016-02-08 22:40:28 +0000

    initial project commit

---
+++ PopChat_Client/PopChat_Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PopChat_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 4172);
+            Console.WriteLine("Conect to server");
+            NetworkStream n = client.GetStream();
+            Console.WriteLine("Connected");
+            string ch = Console.ReadLine();
+            byte[] message = Encoding.ASCII.GetBytes(ch);
+            n.Write(message, 0,message.Length);
+            Console.WriteLine("-----Sent-----");
+
+            client.Close();
+            Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ PopChat_Server/PopChat_Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace PopChat_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listen = new TcpListener(IPAddress.Any, 4172);
+            Console.WriteLine("Listening");
+            listen.Start();
+            TcpClient client = listen.AcceptTcpClient();
+            //when accept
+
+            Console.WriteLine("Client connected");
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            int data = stream.Read(buffer, 0, client.ReceiveBufferSize);
+            string ch = Encoding.ASCII.GetString(buffer, 0, data);
+            Console.WriteLine("Message received" + ch);
+
+            client.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
